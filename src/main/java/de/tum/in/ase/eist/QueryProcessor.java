3b2f22e72ab123e9c0f21a31f8e9d15c3d64ccec
Paul Zhang <me@paul-zhang.de>
2022-07-14 15:19:44 +0200

    remove todo

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -13,8 +13,18 @@ public class QueryProcessor {
                     "writer in the English language and the world's pre-eminent dramatist.";
         } else if (query.contains("name")) {
            return "Paul";
-        } else {
-            return "";
+        } else if (query.contains("what is") && query.contains("plus")){
+            query = query.replace("what is", "").trim();
+            String[] splitted = query.split("plus");
+            if (splitted.length < 2) {
+                return "";
+            } else {
+                int a = Integer.parseInt(splitted[0].trim());
+                int b = Integer.parseInt(splitted[1].trim());
+                return String.valueOf(a + b);
+            }
         }
+
+        return "";
     }
 }
